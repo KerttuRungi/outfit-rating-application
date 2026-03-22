089ce8d70ba2f78d2c3433c10f1c86ab739a24a1
KerttuRungi <kerttu.rungi@gmail.com>
2026-03-22 21:52:23 +0200

    Configure Swagger for testing

--- Outfit-Rating-Backend/Program.cs
+++ Outfit-Rating-Backend/Program.cs
@@ -21,12 +21,16 @@ namespace Outfit_Rating_Backend
 
             // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
             builder.Services.AddOpenApi();
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
 
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
+                app.UseSwagger();
+                app.UseSwaggerUI();
                 app.MapOpenApi();
             }
 
