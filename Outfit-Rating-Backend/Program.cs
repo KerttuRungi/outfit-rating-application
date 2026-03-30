c9889c626b88177ab3b0197d38e63dc59fb9d70f
KerttuRungi <kerttu.rungi@gmail.com>
2026-03-30 23:24:07 +0300

    Identity setup
    Initial endpoints for user login and register

--- Outfit-Rating-Backend/Program.cs
+++ Outfit-Rating-Backend/Program.cs
@@ -1,7 +1,9 @@
 
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using OutfitRating.Application.Interfaces;
 using OutfitRating.Application.Services;
+using OutfitRating.Domain.Entities;
 using OutfitRating.Infrastructure;
 
 namespace Outfit_Rating_Backend
@@ -15,10 +17,27 @@ namespace Outfit_Rating_Backend
             // Add services to the container.
 
             builder.Services.AddControllers();
-            builder.Services.AddScoped<IOutfitRatingService, OutfitRatingService>();
+            builder.Services.AddScoped<IOutfitService, OutfitService>();
             builder.Services.AddScoped<IFileService, FileService>();
+
+
             builder.Services.AddDbContext<AppDbContext>(options =>
                 options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+            builder.Services.AddIdentityApiEndpoints<ApplicationUser>()
+                .AddEntityFrameworkStores<AppDbContext>();
+
+            builder.Services.Configure<IdentityOptions>(options =>
+            {
+                options.Password.RequireDigit = false;
+                options.Password.RequiredLength = 3;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequireLowercase = false;
+                options.SignIn.RequireConfirmedAccount = false;
+            });
+
+            builder.Services.AddAuthorization();
 
             // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
             builder.Services.AddOpenApi();
@@ -35,12 +54,17 @@ namespace Outfit_Rating_Backend
                 app.MapOpenApi();
             }
 
+            app.MapIdentityApi<ApplicationUser>();
+
             app.UseHttpsRedirection();
 
             app.UseStaticFiles();
 
-            app.UseAuthorization();
+            app.UseRouting();
 
+            app.UseAuthentication();
+
+            app.UseAuthorization();
 
             app.MapControllers();
 
--- OutfitRating.Infrastructure/AppDbContext.cs
+++ OutfitRating.Infrastructure/AppDbContext.cs
@@ -3,16 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using OutfitRating.Domain.Entities;
 
 namespace OutfitRating.Infrastructure
 {
-    public class AppDbContext : DbContext
+    public class AppDbContext : IdentityDbContext<ApplicationUser>
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
         }
-        public DbSet<Outfit> OutfitRating { get; set; }
+        public DbSet<Outfit> OutfitRating { get; set; } //Outfit entitys table not to be confused with ratings
     }
 }
